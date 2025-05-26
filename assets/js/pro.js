6e6734bd44c5c744994a578117407f201b335b59
asadbek <asadbekqulboyev@gmail.com>
2025-05-26 16:48:14 +0500

    order vs home desctop finish

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -78,10 +78,13 @@ $(document).ready(function () {
   $('input[name="to"]').on("input", function () {
     $("input[name='to_region']").val($(this).val());
   });
-  $(".tourfirm_tab_btns input[name='tourfirm_type_btn']").on("change",
-    function(){$('input[name="tourfirm_type_copy"]').val($(this).val());}
+  $(".tourfirm_tab_btns input[name='tourfirm_type_btn']").on(
+    "change",
+    function () {
+      $('input[name="tourfirm_type_copy"]').val($(this).val());
+    }
   );
-  $(".tourfirm_tabs input[name='tourfirm_type']").on("change", function (){
+  $(".tourfirm_tabs input[name='tourfirm_type']").on("change", function () {
     $('input[name="tourfirm_car_copy"]').val($(this).val());
   });
   $('input[name="weight"]').on("input", function () {
@@ -97,5 +100,127 @@ $(document).ready(function () {
       $('input[name="weight_copy"]').val("");
     }
   });
-  
+  $('.services_items input[type="checkbox"]').on("change", function () {
+    // Tanlangan checkboxlarning qiymatlarini olish
+    let selected = $('.services_items input[type="checkbox"]:checked')
+      .map(function () {
+        return $(this).val();
+      })
+      .get()
+      .join(", ");
+    $(".sidebar_service input").val(selected);
+  });
+  $('.cargo_calculation_header input[type="checkbox"]').on(
+    "change",
+    function () {
+      // Tanlangan checkboxlarning qiymatlarini olish
+      let selected = $(
+        '.cargo_calculation_header input[type="checkbox"]:checked'
+      );
+      if (selected) {
+        $(".cargo_info_inputs.last").slideUp();
+        $(".cargo_calculation_items").slideDown();
+      } else {
+        $(".cargo_calculation_items").slideUp();
+      }
+    }
+  );
+  let counter = 1;
+
+  function updateBlocks() {
+    $(".cargo_calculation_items .cargo_info_inputs").each(function (index) {
+      const num = index + 1;
+      $(this)
+        .find(".cargo_calculation_items .cargo_calculation_number")
+        .text(num);
+      $(this)
+        .find('input[name^="size"]')
+        .attr("name", "size" + num);
+      $(this)
+        .find('input[name^="weight"]')
+        .attr("name", "weight" + num);
+      $(this)
+        .find('input[name^="sum"]')
+        .attr("name", "sum" + num);
+    });
+
+    // Agar 2+ blok bo‘lsa, o‘chirish tugmasini ko‘rsatamiz
+    if ($(".cargo_calculation_items .cargo_info_inputs").length > 1) {
+      $(".cargo_calculation_items .remove_input_btn").show();
+    } else {
+      $(".cargo_calculation_items .remove_input_btn").hide();
+    }
+  }
+
+  $(".cargo_calculation_items .add_input_btn").on("click", function (e) {
+    e.preventDefault();
+    counter++;
+
+    const newBlock = $(`
+      <div class="cargo_info_inputs">
+        <div class="cargo_calculation_number">${counter}</div>
+        <div class="cargo_info_input">
+          <input type="text" placeholder="Габариты (ДхШхВ), м" name="size_input${counter}">
+        </div>
+        <div class="cargo_info_input">
+          <input type="number" placeholder="Объем, м3" name="wolume_input${counter}">
+        </div>
+        <div class="cargo_info_input">
+          <input type="number" placeholder="Вес, кг" name="weight_input${counter}">
+        </div>
+        <div class="cargo_info_input">
+                          <input
+                            type="number"
+                            placeholder="Количество мест"
+                            name="counter_input"
+                          />
+                        </div>
+      </div>
+    `);
+
+    $(".cargo_calculation_items .cargo_info_inputs").last().after(newBlock);
+    updateBlocks();
+  });
+
+  $(".cargo_info_inputs .remove_input_btn").on("click", function (e) {
+    e.preventDefault();
+    if ($(".cargo_info_inputs .cargo_info_inputs").length > 1) {
+      $(".cargo_info_inputs .cargo_info_inputs").last().remove();
+      counter = $(".cargo_info_inputs .cargo_info_inputs").length;
+      updateBlocks();
+    }
+  });
+  updateBlocks();
+  // tab content
+  $(".tourfirm_tab_content").fadeOut();
+  $(".tourfirm_tab_content#tab_content1").fadeIn();
+  $('input[name="tourfirm_type_btn"]').on("change", function () {
+    const value = $(this).val();
+    let targetId = "";
+
+    switch (value) {
+      case "Еврофура":
+        targetId = "#tab_content1";
+        break;
+      case "Прямая машина":
+        targetId = "#tab_content2";
+        break;
+      case "Сборный груз":
+        targetId = "#tab_content3";
+        break;
+      case "Контейнер":
+        targetId = "#tab_content4";
+        break;
+      case "Негабарит":
+        targetId = "#tab_content5";
+        break;
+    }
+
+    // Agar tanlangan content hozir ko‘rsatilayotgan bo‘lsa, hech narsa qilmaymiz
+    if (!$(targetId).is(":visible")) {
+      $(".tourfirm_tab_content:visible").fadeOut(200, function () {
+        $(targetId).fadeIn(200);
+      });
+    }
+  });
 });
